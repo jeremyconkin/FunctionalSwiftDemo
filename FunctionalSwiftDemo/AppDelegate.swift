ded538e0c81a66ee7cfde58e701f33978b30cdac
Jeremy Conkin <jconkin@gmail.com>
2017-03-27 21:39:40 -0600

    Project Setup

---
+++ FunctionalSwiftDemo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FunctionalSwiftDemo
+//
+//  Created by Jeremy Conkin on 3/27/17.
+//  Copyright © 2017 JeremyConkin. All rights reserved.
+//
+
+import UIKit
+
+/// Item that receives callbacks for app lifecycle and operating system events
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    /// Window in which the application runs
+    var window: UIWindow?
+}
+
---
+++ FunctionalSwiftDemo/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FunctionalSwiftDemo
+//
+//  Created by Jeremy Conkin on 3/27/17.
+//  Copyright © 2017 JeremyConkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
